5763a520001bb3d4e9c0ae19a7b4f2d3b1afc9f8
Dhawal Singh Panwar <ultimatefd838@gmail.com>
2019-03-17 15:50:50 +0530

    Update ProFyle.py

--- ProFyle.py
+++ ProFyle.py
@@ -3,39 +3,51 @@ import GenScreenShot as gss
 import time
 
 import docx
-from docx.shared import Inches,Pt
+from docx.shared import Inches,Pt,Mm
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_TAB_ALIGNMENT
 
-print("Input Required Data")
-UID=input("UID - ")
+#print("Input Required Data")
+UID="17BCS3527"#input("UID - ")
 githandle="@dsp9107"#"@"+input("githandle - ")
-aim="WAP to calculate area of a circle."#input("Aim : ")
+aim="WAP to calculate area of a circle"#input("Aim : ")
 source="Script.py"#input("Name of File - ")
 
 print("Preparing ...")
 time.sleep(0.5)
 
 code=rps.read()
+
 screen=gss.genscreen(source)
 
 print("Working on The Document ...")
+
 doc=docx.Document()
+
+section = doc.sections[0]
+section.page_height = Mm(297)
+section.page_width = Mm(210)
+section.left_margin = Mm(25.4)
+section.right_margin = Mm(25.4)
+section.top_margin = Mm(25.4)
+section.bottom_margin = Mm(25.4)
+section.header_distance = Mm(12.5)
+section.footer_distance = Mm(12.5)
 
 styles = doc.styles
 style = styles.add_style("Head", WD_STYLE_TYPE.PARAGRAPH)
 style.base_style = styles["Normal"]
 style.font.name = 'Calibri'
 tab_stops = style.paragraph_format.tab_stops
-tab_stops.add_tab_stop(Inches(3.25), WD_TAB_ALIGNMENT.CENTER)
-tab_stops.add_tab_stop(Inches(5.75), WD_TAB_ALIGNMENT.RIGHT)
+tab_stops.add_tab_stop(Inches(3.0), WD_TAB_ALIGNMENT.CENTER)
+tab_stops.add_tab_stop(Inches(6.0), WD_TAB_ALIGNMENT.RIGHT)
 
-header = doc.sections[0].header
+header = section.header
 paragraph = header.paragraphs[0]
 paragraph.text = "\t\t"+UID
 paragraph.style = doc.styles["Head"]
 
-footer = doc.sections[0].footer
+footer = section.footer
 paragraph = footer.paragraphs[0]
 paragraph.text = "\t\t"+githandle
 paragraph.style = doc.styles["Head"]
